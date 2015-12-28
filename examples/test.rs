a3d02985478dd9d9db3a7bb7466d08c35e5499f4
mitchmindtree <mitchell.nordine@gmail.com>
2015-12-28 17:35:04 +1100

    Use portaudio directly in example instead of SoundStream

--- examples/test.rs
+++ examples/test.rs
@@ -1,15 +1,16 @@
 //! Read the envelope from the input stream buffer (and pass the input buffer straight to the output).
 
-extern crate dsp;
 extern crate envelope_detector;
+extern crate portaudio as pa;
 extern crate time_calc as time;
 
 use envelope_detector::MultiChannelEnvelopeDetector;
 
-
-use dsp::{CallbackFlags, CallbackResult, Settings, SoundStream, StreamParams};
-
 fn main() {
+    run().unwrap()
+}
+
+fn run() -> Result<(), pa::Error> {
 
     const CHANNELS: u16 = 2;
     const SAMPLE_HZ: f64 = 44_100.0;
@@ -24,15 +25,11 @@ fn main() {
     let mut envelope_detector = MultiChannelEnvelopeDetector::rms(window, attack, release, channels);
 
     // Callback used to construct the duplex sound stream.
-    let callback = Box::new(move |input: &[f32], in_settings: Settings,
-                                  output: &mut[f32], _out_settings: Settings,
-                                  _dt: f64,
-                                  _: CallbackFlags| {
+    let callback = move |pa::DuplexStreamCallbackArgs { in_buffer, out_buffer, frames, .. }| {
 
-        let n_frames = in_settings.frames as usize;
-        let n_channels = in_settings.channels as usize;
-        let sample_hz = in_settings.sample_hz as f64;
-        let window_frames = time::Ms(WINDOW_SIZE_MS).samples(sample_hz) as usize;
+        let n_frames = frames as usize;
+        let n_channels = CHANNELS as usize;
+        let window_frames = time::Ms(WINDOW_SIZE_MS).samples(SAMPLE_HZ) as usize;
         let attack = time::Ms(ATTACK_MS).samples(SAMPLE_HZ) as f32;
         let release = time::Ms(RELEASE_MS).samples(SAMPLE_HZ) as f32;
 
@@ -46,36 +43,36 @@ fn main() {
         for _ in 0..n_frames {
             env.clear();
             for j in 0..n_channels {
-                let sample = input[idx];
+                let sample = in_buffer[idx];
                 let env_sample = envelope_detector.next(j, sample);
                 env.push(env_sample);
                 idx += 1;
             }
-            println!("\nsamples:  {:?}", &input[idx-n_channels..idx]);
+            println!("\nsamples:  {:?}", &in_buffer[idx-n_channels..idx]);
             println!("envelope: {:?}", &env);
         }
 
         // Write the input to the output for fun.
-        for (out_sample, in_sample) in output.iter_mut().zip(input.iter()) {
+        for (out_sample, in_sample) in out_buffer.iter_mut().zip(in_buffer.iter()) {
             *out_sample = *in_sample;
         }
 
-        CallbackResult::Continue
-    });
+        pa::Continue
+    };
 
     // Construct parameters for a duplex stream and the stream itself.
-    let params = StreamParams::new().channels(CHANNELS as i32);
-    let stream = SoundStream::new()
-        .sample_hz(SAMPLE_HZ)
-        .frames_per_buffer(128)
-        .duplex(params, params)
-        .run_callback(callback)
-        .unwrap();
+    const FRAMES: u32 = 128;
+    let pa = try!(pa::PortAudio::new());
+    let chan = CHANNELS as i32;
+    let settings = try!(pa.default_duplex_stream_settings(chan, chan, SAMPLE_HZ, FRAMES));
+    let mut stream = try!(pa.open_non_blocking_stream(settings, callback));
+    try!(stream.start());
 
     // Wait for our stream to finish.
-    while let Ok(true) = stream.is_active() {
+    while let true = try!(stream.is_active()) {
         ::std::thread::sleep_ms(16);
     }
 
+    Ok(())
 }
 
